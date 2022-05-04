c5d5d49d739a40653ae92ee14eb041976cba1e29
Austin Bodin <austin.bodin@gmail.com>
2022-05-04 11:28:15 -0500

    protected backend routes for events/games/groups

--- server/routes/games.js
+++ server/routes/games.js
@@ -3,76 +3,86 @@ const pool = require("../db");
 const router = express.Router();
 
 // create a game
-router.post("/", async (req, res) => {
-  try {
-    console.log(req.body);
-    const { game_name, game_desc, min_players, max_players, game_dur } =
-      req.body;
-    const newGame = await pool.query(
-      "INSERT INTO games (game_name, game_desc, min_players, max_players, game_dur) VALUES($1, $2, $3, $4, $5) RETURNING *",
-      [game_name, game_desc, min_players, max_players, game_dur]
-    );
-    res.json(newGame.rows[0]);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.post("/", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.body);
+      const { game_name, game_desc, min_players, max_players, game_dur } =
+        req.body;
+      const newGame = await pool.query(
+        "INSERT INTO games (game_name, game_desc, min_players, max_players, game_dur) VALUES($1, $2, $3, $4, $5) RETURNING *",
+        [game_name, game_desc, min_players, max_players, game_dur]
+      );
+      res.json(newGame.rows[0]);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 // get all games
-router.get("/", async (req, res) => {
-  try {
-    const allGames = await pool.query("SELECT * FROM games");
-    res.json(allGames.rows);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.get("/", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      const allGames = await pool.query("SELECT * FROM games");
+      res.json(allGames.rows);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 // get a single game
-router.get("/:game_id", async (req, res) => {
-  try {
-    console.log(req.params);
-    const { game_id } = req.params;
-    const singleGame = await pool.query(
-      "SELECT * FROM games WHERE game_id = $1",
-      [game_id]
-    );
-    res.json(singleGame.rows[0]);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.get("/:game_id", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.params);
+      const { game_id } = req.params;
+      const singleGame = await pool.query(
+        "SELECT * FROM games WHERE game_id = $1",
+        [game_id]
+      );
+      res.json(singleGame.rows[0]);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 // update a game
-router.put("/:game_id", async (req, res) => {
-  try {
-    console.log(req.params);
-    console.log(req.body);
-    const { game_id } = req.params;
-    const { game_name, game_desc, min_players, max_players, game_dur } =
-      req.body;
-    const singleGame = await pool.query(
-      "UPDATE games SET game_name=$2, game_desc=$3, min_players=$4, max_players=$5, game_dur=$6 WHERE game_id = $1",
-      [game_id, game_name, game_desc, min_players, max_players, game_dur]
-    );
-    res.json(`${game_name} was updated!`);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.put("/:game_id", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.params);
+      console.log(req.body);
+      const { game_id } = req.params;
+      const { game_name, game_desc, min_players, max_players, game_dur } =
+        req.body;
+      const singleGame = await pool.query(
+        "UPDATE games SET game_name=$2, game_desc=$3, min_players=$4, max_players=$5, game_dur=$6 WHERE game_id = $1",
+        [game_id, game_name, game_desc, min_players, max_players, game_dur]
+      );
+      res.json(`${game_name} was updated!`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 // delete a game
-router.delete("/:game_id", async (req, res) => {
-  try {
-    const { game_id } = req.params;
-    const deleteGame = await pool.query(
-      "DELETE FROM games WHERE game_id = $1",
-      [game_id]
-    );
-    res.json(`Game was deleted!`);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.delete("/:game_id", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      const { game_id } = req.params;
+      const deleteGame = await pool.query(
+        "DELETE FROM games WHERE game_id = $1",
+        [game_id]
+      );
+      res.json(`Game was deleted!`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 module.exports = router;
--- server/routes/groups.js
+++ server/routes/groups.js
@@ -3,183 +3,207 @@ const pool = require("../db");
 const router = express.Router();
 
 // create a group
-router.post("/", async (req, res) => {
-  try {
-    console.log(req.body);
-    const { group_name } = req.body;
-    const newGroup = await pool.query(
-      'INSERT INTO "game-group" (group_name) VALUES($1) RETURNING *',
-      [group_name]
-    );
-    res.json(newGroup.rows[0]);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.post("/", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.body);
+      const { group_name } = req.body;
+      const newGroup = await pool.query(
+        'INSERT INTO "game-group" (group_name) VALUES($1) RETURNING *',
+        [group_name]
+      );
+      res.json(newGroup.rows[0]);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 // get all groups
-router.get("/", async (req, res) => {
-  try {
-    const allGroups = await pool.query('SELECT * FROM "game-group"');
-    res.json(allGroups.rows);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.get("/", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      const allGroups = await pool.query('SELECT * FROM "game-group"');
+      res.json(allGroups.rows);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 // get a single group
-router.get("/:group_id", async (req, res) => {
-  try {
-    console.log(req.params);
-    const { group_id } = req.params;
-    const singleGroup = await pool.query(
-      'SELECT * FROM "game-group" WHERE group_id = $1',
-      [group_id]
-    );
-    res.json(singleGroup.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
+router.get("/:group_id", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.params);
+      const { group_id } = req.params;
+      const singleGroup = await pool.query(
+        'SELECT * FROM "game-group" WHERE group_id = $1',
+        [group_id]
+      );
+      res.json(singleGroup.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+    }
+  })(req, res);
 });
 
 // get all players in a group using the the group_id
-router.get("/:group_id/players", async (req, res) => {
-  try {
-    console.log(req.params);
-    const { group_id } = req.params;
-    const groupPlayers = await pool.query(
-      'SELECT * FROM "group_players" LEFT JOIN players ON group_players.player_id = players.player_id WHERE group_id = $1',
-      [group_id]
-    );
-    res.json(groupPlayers.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
+router.get("/:group_id/players", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.params);
+      const { group_id } = req.params;
+      const groupPlayers = await pool.query(
+        'SELECT * FROM "group_players" LEFT JOIN players ON group_players.player_id = players.player_id WHERE group_id = $1',
+        [group_id]
+      );
+      res.json(groupPlayers.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+  })(req, res);
 });
 
 // get all games in a group using the group_id
-router.get("/:group_id/games", async (req, res) => {
-  try {
-    console.log(req.params);
-    const { group_id } = req.params;
-    const groupPlayers = await pool.query(
-      'SELECT * FROM "group_games" LEFT JOIN games ON group_games.game_id = games.game_id WHERE group_id = $1',
-      [group_id]
-    );
-    res.json(groupPlayers.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
+router.get("/:group_id/games", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.params);
+      const { group_id } = req.params;
+      const groupPlayers = await pool.query(
+        'SELECT * FROM "group_games" LEFT JOIN games ON group_games.game_id = games.game_id WHERE group_id = $1',
+        [group_id]
+      );
+      res.json(groupPlayers.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+  })(req, res);
 });
 
 // add a game to the group inventory using the group_id and game_id
-router.post("/:group_id/games", async (req, res) => {
-  try {
-    console.log(req.body);
-    const { group_id } = req.params;
-    const { game_id } = req.body;
-    const newGame = await pool.query(
-      'INSERT INTO "group_games" (group_id, game_id) VALUES($1, $2) RETURNING *',
-      [group_id, game_id]
-    );
-    res.json(newGame.rows[0]);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.post("/:group_id/games", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.body);
+      const { group_id } = req.params;
+      const { game_id } = req.body;
+      const newGame = await pool.query(
+        'INSERT INTO "group_games" (group_id, game_id) VALUES($1, $2) RETURNING *',
+        [group_id, game_id]
+      );
+      res.json(newGame.rows[0]);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 // delete a game from the group inventory using the group_id and game_id
-router.delete("/:group_id/games", async (req, res) => {
-  try {
-    console.log(req.body);
-    const { group_id } = req.params;
-    const { game_id } = req.body;
-    const deleteGame = await pool.query(
-      'DELETE FROM "group_games" WHERE group_id = $1 AND game_id = $2',
-      [group_id, game_id]
-    );
-    res.json(`Game was deleted!`);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.delete("/:group_id/games", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.body);
+      const { group_id } = req.params;
+      const { game_id } = req.body;
+      const deleteGame = await pool.query(
+        'DELETE FROM "group_games" WHERE group_id = $1 AND game_id = $2',
+        [group_id, game_id]
+      );
+      res.json(`Game was deleted!`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 // add a player to the group using the group_id and player_id
-router.post("/:group_id/players", async (req, res) => {
-  try {
-    console.log(req.body);
-    const { group_id } = req.params;
-    const { player_id } = req.body;
-    const newPlayer = await pool.query(
-      'INSERT INTO "group_players" (group_id, player_id) VALUES($1, $2) RETURNING *',
-      [group_id, player_id]
-    );
-    res.json(newPlayer.rows[0]);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.post("/:group_id/players", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.body);
+      const { group_id } = req.params;
+      const { player_id } = req.body;
+      const newPlayer = await pool.query(
+        'INSERT INTO "group_players" (group_id, player_id) VALUES($1, $2) RETURNING *',
+        [group_id, player_id]
+      );
+      res.json(newPlayer.rows[0]);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 // delete a player from the group roster using the group_id and player_id
-router.delete("/:group_id/players", async (req, res) => {
-  try {
-    console.log(req.body);
-    const { group_id } = req.params;
-    const { player_id } = req.body;
-    const deletePlayer = await pool.query(
-      'DELETE FROM "group_players" WHERE group_id = $1 AND player_id = $2',
-      [group_id, player_id]
-    );
-    res.json(`Player was deleted!`);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.delete("/:group_id/players", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.body);
+      const { group_id } = req.params;
+      const { player_id } = req.body;
+      const deletePlayer = await pool.query(
+        'DELETE FROM "group_players" WHERE group_id = $1 AND player_id = $2',
+        [group_id, player_id]
+      );
+      res.json(`Player was deleted!`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 // update a group
-router.put("/:group_id", async (req, res) => {
-  try {
-    console.log(req.params);
-    console.log(req.body);
-    const { group_id } = req.params;
-    const { group_name } = req.body;
-    const singleGroup = await pool.query(
-      'UPDATE "game-group" SET game_name=$2, game_desc=$3, min_players=$4, max_players=$5, game_dur=$6 WHERE game_id = $1',
-      [group_id, group_name]
-    );
-    res.json(`Group was updated!`);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.put("/:group_id", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.params);
+      console.log(req.body);
+      const { group_id } = req.params;
+      const { group_name } = req.body;
+      const singleGroup = await pool.query(
+        'UPDATE "game-group" SET game_name=$2, game_desc=$3, min_players=$4, max_players=$5, game_dur=$6 WHERE game_id = $1',
+        [group_id, group_name]
+      );
+      res.json(`Group was updated!`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 // delete a group
-router.delete("/:group_id", async (req, res) => {
-  try {
-    const { group_id } = req.params;
-    const deleteGame = await pool.query(
-      'DELETE FROM "game-group" WHERE group_id = $1',
-      [group_id]
-    );
-    res.json(`Group was deleted!`);
-  } catch (error) {
-    console.error(error.message);
-  }
+router.delete("/:group_id", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      const { group_id } = req.params;
+      const deleteGame = await pool.query(
+        'DELETE FROM "game-group" WHERE group_id = $1',
+        [group_id]
+      );
+      res.json(`Group was deleted!`);
+    } catch (error) {
+      console.error(error.message);
+    }
+  })(req, res);
 });
 
 // get all events for a group using the group_id
-router.get("/:group_id/events", async (req, res) => {
-  try {
-    console.log(req.params);
-    const { group_id } = req.params;
-    const groupEvents = await pool.query(
-      'SELECT * FROM "events" WHERE group_id = $1',
-      [group_id]
-    );
-    res.json(groupEvents.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
+router.get("/:group_id/events", (req, res) => {
+  passport.authenticate("jwt", { session: false }, async (err, user) => {
+    try {
+      console.log(req.params);
+      const { group_id } = req.params;
+      const groupEvents = await pool.query(
+        'SELECT * FROM "events" WHERE group_id = $1',
+        [group_id]
+      );
+      res.json(groupEvents.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+  })(req, res);
 });
 
 module.exports = router;
